34a22f6fea78f7315d7b5f4a3a6280d45ebb238c
AbdalQader mhemed <abdalqader.mhemed@gmail.com>
2021-05-01 22:44:51 +0300

    first initialize spring project

---
+++ src/main/java/com/example/tabadol/TabadolApplication.java
@@ -0,0 +1,13 @@
+package com.example.tabadol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabadolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabadolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tabadol/TabadolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tabadol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabadolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
