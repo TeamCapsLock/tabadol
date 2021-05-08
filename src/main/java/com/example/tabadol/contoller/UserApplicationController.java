f1e34973cbe64dcae4ed9305e7b089bdb0619110
MHD22 <mhd.sad22@gmail.com>
2021-05-08 23:32:23 +0300

    solve the image url issue

--- src/main/java/com/example/tabadol/contoller/UserApplicationController.java
+++ src/main/java/com/example/tabadol/contoller/UserApplicationController.java
@@ -159,6 +159,10 @@ public class UserApplicationController {
     @PostMapping("/edit-profile/{id}")
     public RedirectView editProfile(@PathVariable(value="id") long id,String firstname, String lastname, String skills, String bio, String phone, String image){
 
+        final String imageUrl = "https://res.cloudinary.com/saify/image/upload/v1539009756/icon.jpg";
+        //if the user did not enter an url image
+        if(image.length() == 0)
+            image = imageUrl;
         UserApplication userToEdit = userApplicationRepository.findById(id).get();
         userToEdit.setFirstname(firstname);
         userToEdit.setLastname(lastname);
