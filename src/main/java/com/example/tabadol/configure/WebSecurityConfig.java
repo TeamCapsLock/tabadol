a6b26090c94f9c7a2a9963d52c0752b806e2fa93
Mohamad Saad Eddin <47499938+MHD22@users.noreply.github.com>
2021-06-12 21:39:29 +0300

    Update WebSecurityConfig.java

--- src/main/java/com/example/tabadol/configure/WebSecurityConfig.java
+++ src/main/java/com/example/tabadol/configure/WebSecurityConfig.java
@@ -34,7 +34,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .cors().disable()
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers( "/login", "/signup","/","/js/**","/assets/**","/fragments/**")
+                .antMatchers("/**", "/login", "/signup","/","/js/**","/assets/**","/fragments/**")
                 .permitAll().anyRequest().authenticated()
                 .and().formLogin().loginPage("/login")
                 .loginProcessingUrl("/login")
